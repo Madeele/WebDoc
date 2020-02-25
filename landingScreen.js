7e615f7c3f30dedce110f3c55df4631809928f51
Greg <genius242424@gmail.com>
2020-02-25 16:03:23 +0100

    sounds added

--- landingScreen.js
+++ landingScreen.js
@@ -1,5 +1,12 @@
 
 function landingScreen() {
-  var element = document.getElementById("landingScreen");
-  element.classList.add("disappear");
+    
+    let element = document.getElementById("landingScreen");
+    element.classList.add("disappear");
+    
+    let sound  = document.getElementById("unlock");
+    sound.play();
+    
+    let ambient  = document.getElementById("ambient");
+    ambient.play();
 }
