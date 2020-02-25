9d5fad6663f776fed84b5b36e8b52060e311a59e
Magdalena <polakowska.mag@gmail.com>
2020-02-25 14:49:19 +0100

    YASS IT WORKS

--- playbutton.js
+++ playbutton.js
@@ -6,8 +6,6 @@ myVideo.controls = false;
 function playPause() {
   if (myVideo.paused)
     myVideo.play();
-    //myVideo.controls = true;
   else
     myVideo.pause();
-    //myVideo.controls = true;
 }
